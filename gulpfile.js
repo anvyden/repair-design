905f2ffaa34814bb4c57ebc56c794952186a5ab6
Denis Narkevich <@anvyden@gmail.com@>
2020-03-04 23:22:39 +0700

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const minimizecss = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
-
-gulp.task('hello', function(done) {
-  console.log('Привет, мир!');
-  done();
-});
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
-gulp.task('minimizecss', function() {
-  return gulp.src('./css/*.css')
-  .pipe(minimizecss())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('./css'))
-});
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
+
+// function mincss() {
+//   return src('./css/*.css')
+//   .pipe(minimizecss())
+//   .pipe(rename({suffix: '.min'}))
+//   .pipe(dest('./css'))
+// };
