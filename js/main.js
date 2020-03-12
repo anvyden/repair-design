93ca3d9029179d1a38079aa56662bd463ef61138
Denis Narkevich <@anvyden@gmail.com@>
2020-03-12 13:23:17 +0700

    feat: add function close modal window on click and keypress Esc

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  const modalDialog = document.querySelector('.modal__dialog');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
@@ -10,5 +11,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
     closeBtn.addEventListener('click', switchModal);
+
+    modal.addEventListener('click', (event) => {
+      // let target = event.target;
+      if((event.target).closest('.modal__dialog'))
+      event.stopPropagation();
+      else if((event.target).closest('.modal'))
+      modal.classList.remove('modal--visible');
+    })
+
+    document.addEventListener('keydown', (event) => {
+      if (event.keyCode === 27)
+      modal.classList.remove('modal--visible');
+    })
+  
+    // modal.addEventListener('click', (event) => {
+
+    //   event.stopPropagation();
+    //   modal.classList.remove('modal--visible');
+    // });
+
+    // modalDialog.addEventListener('click', (event) => {
+
+    //   event.stopPropagation();
+    //   modal.classList.remove('modal--visible');
+    // });
+
+    // document.addEventListener('click', (event) => {
+    // if (!(event.target).closest('.modal').length && !(event.target).is('.modal')) {
+    // switchModal() }});
     
 });
