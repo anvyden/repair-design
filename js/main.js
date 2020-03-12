1df5b5b4d564bac41022c1cd3c68f68fafd3e4d6
Denis Narkevich <@anvyden@gmail.com@>
2020-03-12 13:26:32 +0700

    refact: remove unnecessary code in main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     closeBtn.addEventListener('click', switchModal);
 
     modal.addEventListener('click', (event) => {
-      // let target = event.target;
       if((event.target).closest('.modal__dialog'))
       event.stopPropagation();
       else if((event.target).closest('.modal'))
@@ -25,20 +24,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
       modal.classList.remove('modal--visible');
     })
   
-    // modal.addEventListener('click', (event) => {
-
-    //   event.stopPropagation();
-    //   modal.classList.remove('modal--visible');
-    // });
-
-    // modalDialog.addEventListener('click', (event) => {
-
-    //   event.stopPropagation();
-    //   modal.classList.remove('modal--visible');
-    // });
-
-    // document.addEventListener('click', (event) => {
-    // if (!(event.target).closest('.modal').length && !(event.target).is('.modal')) {
-    // switchModal() }});
-    
 });
