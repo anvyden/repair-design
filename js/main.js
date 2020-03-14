85506d558ed597ccbfd4a789102a49092bb6b804
Denis Narkevich <@anvyden@gmail.com@>
2020-03-14 22:33:24 +0700

    feat: add mobile version projects section

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
   });
   
   $(document).scroll(function() {
-    if ($(this).scrollTop() > 30) {
+    if ($(this).scrollTop() > 30 && $(window).width() >= 760) {
       if($('.button-up').is(':hidden')) {
         $('.button-up').css({opacity : 1}).fadeIn('slow');
       }
@@ -60,7 +60,6 @@ $(document).ready(function () {
   $('.button-up').click(function() {
     $('html, body').stop().animate({scrollTop : 0}, 700);
   });
-
 
   var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
@@ -74,12 +73,48 @@ $(document).ready(function () {
     },
   });
 
+  // $(window).resize(function() {
+  //   if ($(window).width() <= 760) {
+  //     $('.col-60').removeClass('.col-60--top');
+  //   };
+  // });
+
   var next = $('.swiper-button-next');
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
   var bullet = $('.swiper-pagination.bullet')
 
-  next.css('left', prev.width() + 20 + bullets.width() + 20)
-  bullets.css('left', prev.width() + 20)
+  // next.css('left', prev.width() + 20 + bullets.width() + 20)
+  // bullets.css('left', prev.css('left') + prev.width() + 20)
+  
+  // Способ перемещения блока из другого блока при определенном разрешении
+  // $(window).resize(function () {
+  //   if($(window).width() <= 760) {
+  //     $('.swiper-container-mobile').detach().prependTo('.col-60-mobile');
+  //   } else {
+  //       $('.col-60').append($('.swiper-container-mobile'));
+  //     }
+
+  // });
+
+  // $(window).resize(function () {
+  //   if ($(window).width() <= 760) {
+  //     // $('.projects__section-title').after($('.swiper-container-mobile')),
+  //     $('.swiper-slide__img').each(function () {
+  //       $(this).attr('src', 'img/project/project-mobile.jpg');
+  //       });
+  //     // $('.swiper-slide__img').attr.each('src', 'img/project/project-mobile.jpg');
+  //   } else {
+  //     // $('.col-60').append($('.swiper-container-mobile'));
+  //     $('.swiper-slide__img').each(function () {
+  //       $(this).attr('src', 'img/project/project.jpg');
+  //       });
+  //   }
+  //   });
+
+    var positionPrev = prev.position();
+
+    bullets.css('left', positionPrev.left + prev.width() + 20)
+    next.css('left', positionPrev.left + prev.width() + 20 + bullets.width() + 20)
 
 });
